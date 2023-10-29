9a43d0fca7796aa0a3099e8c36c554bfb8690a41
Artem Chashchin <chashchin.am@phystech.edu>
2023-10-29 13:10:19 +0500

    get_db added

--- database/db.py
+++ database/db.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from config import cfg
 
-engine = create_engine(
+sync_engine = create_engine(
     url=cfg.database_url_psycopg,
     echo=True
 )
+
+SessionLocal = sessionmaker(bind=sync_engine)
+
+
+def get_db():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
